8ca468329de7c4a2b969f0cdef1a696b9a5701d3
Firas <firas.messaoud@etu.u-bordeaux.fr>
2023-02-22 22:20:12 +0100

    sign in + sign up page created with basic field controls reset password page added logout redirection to login page

--- Quasar-version/src/router/routes.js
+++ Quasar-version/src/router/routes.js
@@ -4,6 +4,16 @@ const routes = [
     component: () => import("layouts/MainLayout.vue"),
     children: [
       { path: "", component: () => import("pages/Home.vue"), name: "Home" },
+      {
+        path: "login",
+        component: () => import("pages/Login.vue"),
+        name: "Login",
+      },
+      {
+        path: "reset-password",
+        component: () => import("pages/ResetPassword.vue"),
+        name: "ResetPassword",
+      },
     ],
   },
 
