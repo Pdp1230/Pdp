3ad6163f7c067df1fb8e97e232e139bd3aa06d18
Ismail El Aissate <ismail.el-aissate@etu.u-bordeaux.fr>
2023-03-09 01:27:51 +0100

    css customizable par l'utilisateur

--- Quasar-version/src/pages/Submit.vue
+++ Quasar-version/src/pages/Submit.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
-    <div v-if="formData">
-      <h1>{{ formData.forms[0].title }}</h1>
-      <form>
-        <div v-for="(question, index) in formData.forms[0].questions" :key="index">
-          <label>{{ question.modelQ }}</label>
+    <div v-if="formData" class="q-pa-md">
+      <h1 class="q-mb-md">{{ formData.forms[0].title }}</h1>
+      <form class="q-form">
+        <div v-for="(question, index) in formData.forms[0].questions" :key="index" class="q-mb-md">
+          <label class="q-mb-sm">{{ question.modelQ }}</label>
           <br>
-          <component :is="question.component" v-model="question.response.value" :options="question.options" />
+          <component :is="question.component" v-model="question.response.value" :options="question.options" class="q-input" />
         </div>
+        <q-btn label="Submit" class="q-mt-lg" @click.prevent="applyStyle()" />
       </form>
     </div>
+    <div v-if="formData">
+  <div class="q-mb-lg">
+    <label class="q-mb-sm">Custom CSS</label>
+    <br>
+    <textarea v-model="style" class="q-input"></textarea>
+  </div>
+  <q-btn label="Submit" class="q-mt-lg" @click.prevent="applyStyle()" />
+</div>
     <input type="file" accept=".json" id="json-input">
   </div>
 </template>
@@ -26,7 +35,8 @@ export default {
   data() {
     return {
       questionOptions: [1, 2, 3, 4, 5],
-      formData: null
+      formData: null,
+      style: ""
     };
   },
   mounted() {
@@ -60,6 +70,13 @@ export default {
         });
       };
     });
+  },
+  methods: {
+  applyStyle() {
+    const styleTag = document.createElement("style");
+    styleTag.innerHTML = this.style;
+    document.head.appendChild(styleTag);
   }
+}
 };
 </script>
