9db5f511e342f7bbe27dc9d19b78885a73b81131
Ismail El Aissate <ismail.el-aissate@etu.u-bordeaux.fr>
2023-03-21 01:48:10 +0100

    Progress Mail

--- Quasar-version/src/pages/Submit.vue
+++ Quasar-version/src/pages/Submit.vue
@@ -2,22 +2,28 @@
   <div>
     <div v-if="formData" class="q-pa-md" :style="formStyle">
       <h1 class="q-mb-md">{{ formData.forms[0].title }}</h1>
-      <form class="q-form">
+      <form class="q-form" @submit.prevent="submitForm">
         <div v-for="(question, index) in formData.forms[0].questions" :key="index" class="q-mb-md">
           <label class="q-mb-sm">{{ question.modelQ }}</label>
           <br>
           <component :is="question.component" v-model="question.response.value" :options="question.options" class="q-input" />
         </div>
-        <q-btn label="Submit" class="q-mt-lg" />
+        <div class="q-mb-md">
+          <label class="q-mb-sm">Email Address</label>
+          <input type="email" v-model="userEmail" class="q-input" required />
+        </div>
+        <q-btn label="Submit" type="submit" class="q-mt-lg" />
       </form>
     </div>
-    <div v-if="formData"></div>
-    <input type="file" accept=".json" id="json-input">
+    <div v-else>
+      <input type="file" accept=".json" id="json-input" @change="loadFormData" />
+    </div>
   </div>
 </template>
 <script>
 import RadioList from "../components/RadioList.vue";
 import { ref } from 'vue';
+import axios from 'axios';
 
 export default {
   name: "formResponse",
@@ -29,10 +35,11 @@ export default {
       questionOptions: [1, 2, 3, 4, 5],
       formData: null,
       formStyle: "",
+      userEmail: "",
     };
   },
-  mounted() {
-    document.getElementById("json-input").addEventListener("change", (event) => {
+  methods: {
+    loadFormData(event) {
       const file = event.target.files[0];
       const reader = new FileReader();
       reader.readAsText(file);
@@ -40,11 +47,6 @@ export default {
         const json = reader.result;
         this.formData = JSON.parse(json);
         this.formData.forms.forEach((form) => {
-          // Supprimer l'élément de style précédent avant d'en ajouter un nouveau
-          const prevStyleElem = document.querySelector("#form-style");
-          if (prevStyleElem) {
-            document.head.removeChild(prevStyleElem);
-          }
           const styleElem = document.createElement("style");
           styleElem.id = "form-style";
           styleElem.innerHTML = form.style.replace(/\n/g, "");
@@ -70,8 +72,28 @@ export default {
         });
         this.formStyle = "";
       };
-    });
+    },
+    async submitForm() {
+      const formData = this.formData.forms[0];
+      const data = formData.questions.reduce((acc, question) => {
+        acc[question.modelQ] = question.response ? question.response.value : null;
+        return acc;
+      }, {});
+
+      try {
+        //Il faut implémenté coté backend l'envoye de mail et la gestion serveur ou bien utiliser un API d'envoye de mail
+        const response = await axios.post('/api/sendmail', {
+          ownerEmail: formData.ownersemail,
+          userEmail: this.userEmail,
+          formTitle: formData.title,
+          formData: data
+        });
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+        throw new Error("Error sending email.");
+      }
+    },
   },
-  methods: {},
 };
 </script>
